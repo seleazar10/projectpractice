4d08e2c05a92cb1f575f45a686c4923628e3c52f
Stama Eleazar <eleoatam10@gmail.com>
2019-07-27 09:15:25 -0500

    link images

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 
-$(document).load(function(){
+$(document).ready(function(){
 
     var search = "workout"
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    search + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    search + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=1";
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response){
-        console.log('mo')
+        console.log(response.data)
+        var imag = response.data
+
+        for(var i = 0; i<imag.length; i++)
+        var img = imag[i].images.fixed_height.url;
+
+
+        console.log('ok')
+
+        $('')
+
     })
 })
